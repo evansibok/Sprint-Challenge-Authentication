319ba835d03bdc7e5c7b2ff583271775195019f7
Evans Ibok <evansibok@gmail.com>
2020-01-31 09:50:28 +0100

    Create auth registration router

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,24 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+
+const UsersDb = require('../users/users-model');
 
 router.post('/register', (req, res) => {
   // implement registration
+  let { username, password } = req.body;
+  const hash = bcrypt.hashSync(password, 10);
+
+  UsersDb.addUser({ username, password: hash })
+    .then(savedUser => {
+      res.status(201).json(savedUser);
+    })
+    .catch(err => {
+      res.status(500).json({
+        errorMessage: "Account creation failed!",
+        stack: err.stack,
+      })
+    });
+
 });
 
 router.post('/login', (req, res) => {
